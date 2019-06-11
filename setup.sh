a9ff608a16eb66ab50f1314f3ba2ad38ce8d6465
Tomasz Martysk <tomek@martynski.pl>
2019-06-11 17:46:11 +0200

    yeee

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 rm -rf ./.caroot/*.pem
 
+docker network create web
+
 CAROOT=.caroot ./.bin/mkcert -uninstall
 CAROOT=.caroot ./.bin/mkcert -install > /dev/null 2>&1
 CAROOT=.caroot ./.bin/mkcert -install
-CAROOT=.caroot .bin/mkcert -cert-file ./volumes/certs/wildcard_loc.crt -key-file ./volumes/certs/wildcard_loc.key "*.aaa.loc"
+CAROOT=.caroot .bin/mkcert -cert-file ./volumes/certs/wildcard_loc.crt -key-file ./volumes/certs/wildcard_loc.key "*.dev.loc"
